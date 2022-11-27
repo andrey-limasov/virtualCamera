139825810457481c2f0b20c1dd5fd71e5d01e1ed
Andrey Limasov <a.limasov@samsung.com>
2022-11-27 13:16:05 +0300

    User Auth

--- cameraadmin/urls.py
+++ cameraadmin/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('archive/', views.archive, name='archive'),
+    path('download/', views.download_video_file, name='download_video_file'),
 ]
--- cameraadmin/views.py
+++ cameraadmin/views.py
@@ -1,3 +1,7 @@
+import mimetypes
+import os
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -15,3 +19,19 @@ def archive(request):
     # Generate counts of some of the main objects
 
     return render(request, 'archive.html')
+
+def download_video_file(request):
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define the full file path
+    filepath = BASE_DIR + '/cameraadmin/static/video/video.mp4'
+    # Open the file for reading content
+    path = open(filepath, 'rb')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=video.mp4"
+    # Return the response value
+    return response
